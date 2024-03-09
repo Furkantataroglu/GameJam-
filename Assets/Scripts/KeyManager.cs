c10b0cf066ebe22dd0274371d6b965561912b9cb
furkanttrgl <104869273+Furkantataroglu@users.noreply.github.com>
2024-03-09 12:26:22 +0300

    door and key

---
+++ Assets/Scripts/KeyManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject door;
+    public bool isPickedup;
+    private Vector2 vel;
+    public float smoothTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(isPickedup)
+       {
+
+    
+       }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //trigger with player
+        if(other.gameObject.CompareTag("Player") && !isPickedup)
+        {
+            isPickedup=true;
+        }
+        
+    }
+}
